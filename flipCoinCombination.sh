ed1d17e357354249c17b61bdeb7d53d734e5fe26
s8189882 <s8189882@gmail.com>
2022-08-03 20:13:41 +0530

    Displaying welcome message

---
+++ flipCoinCombination.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+
+#Start with Displaying Welcome message
+
+echo ""
+echo "Welcome to Flip Coin Combination Program "
+echo "This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
+echo ""
